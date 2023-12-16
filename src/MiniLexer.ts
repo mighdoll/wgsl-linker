a7ee4d2c499319812ceccdbdbe053b27619fe4bb
lee mighdoll <lee@underneath.ca>
2023-12-16 01:08:23 -0800

    peek(), pushMatcher() and popMatcher()

--- src/MiniLexer.ts
+++ src/MiniLexer.ts
@@ -29,22 +29,54 @@ const eol = /$/;
 const line = tokenMatcher({
   directive,
   notDirective,
-  eol
-});
+
+export interface Lexer {
+  next(): Token | undefined;
+  peek(): Token | undefined;
+  pushMatcher(newMatcher: TokenMatcher): void;
+  popMatcher(): void;
+}
 
 export function lex(src: string): Lexer {
-  mainMatch.start(src);
+  const matcherStack: TokenMatcher[] = [];
+  let matcher = mainMatch;
+  const peeked: (Token | undefined)[] = [];
+
+  matcher.start(src);
 
   function next(): Token | undefined {
-    let token = mainMatch.next();
-    while (token?.kind === "ws") {
-      token = mainMatch.next();
+    if (peeked.length > 0) {
+      return peeked.shift();
+    } else {
+      let token = matcher.next();
+      while (token?.kind === "ws") {
+        token = matcher.next();
+      }
+      return token;
     }
-    return token;
   }
 
-  // TODO parse differently inside a comment
-  return { next };
+  function peek(): Token | undefined {
+    if (peeked.length === 0) {
+      peeked.push(next());
+    }
+    return peeked[0];
+  }
+
+  function pushMatcher(newMatcher: TokenMatcher): void {
+    const position = matcher.position();
+    matcherStack.push(matcher);
+    newMatcher.start(src, position);
+    matcher = newMatcher;
+  }
+
+  function popMatcher(): void {
+    const position = matcher.position();
+    matcher = matcherStack.pop() || (console.error("too many pops"), mainMatch);
+    matcher.start(src, position);
+  }
+
+  return { next, peek, pushMatcher, popMatcher };
 }
 
 export function regexOr(flags: string, ...exp: RegExp[]): RegExp {
