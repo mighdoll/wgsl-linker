6ccd75d54d48dc451fcbd65a2cf71158da265d39
lee mighdoll <lee@underneath.ca>
2023-12-26 23:58:50 -0800

    report start/end w/export

--- src/MiniParser.ts
+++ src/MiniParser.ts
@@ -1,3 +1,4 @@
+import { E } from "vitest/dist/types-dea83b3d.js";
 import { Lexer, matchingLexer } from "./MatchingLexer.js";
 import {
   directiveArgsMatch,
@@ -25,7 +26,8 @@ export type AbstractElem = ExportElem;
 /** 'interesting' elements found in the source */
 export interface AbstractElemBase {
   kind: string;
-  position: number;
+  start: number;
+  end: number;
 }
 
 export interface ExportElem extends AbstractElemBase {
@@ -51,8 +53,11 @@ const exportDirective = seq(
     seq(opt(kind("word").named("exportName")), opt(directiveArgs.named("args")))
   )
 ).mapResults((r, state) => {
+  const { start, end } = r;
   const { exportName, args } = r.named;
-  state.results.push({ kind: "export", name: exportName?.[0], args });
+  const name = exportName?.[0];
+  const e: ExportElem = { kind: "export", name, args, start, end };
+  state.results.push(e);
 });
 
 export const directive = exportDirective; // TODO import directive
