a40e257bf7c5aeca3507a3bdc056c03cb4989cf7
lee mighdoll <lee@underneath.ca>
2024-02-04 17:06:57 -0800

    test for mulitple replaces

--- src/test/Replacer.test.ts
+++ src/test/Replacer.test.ts
@@ -1,21 +1,27 @@
-import {test,expect} from "vitest";
+import { test, expect } from "vitest";
 import { replacer } from "../Replacer.js";
 import { dlog } from "berry-pretty";
 
 test("parse blank line", () => {
   const src = "";
-  const result = replacer(src, {})
+  const result = replacer(src, {});
   expect(result).eq(src);
-})
+});
 
 test("parse line w/o replacement", () => {
   const src = "fn main() { // foo";
-  const result = replacer(src, {})
+  const result = replacer(src, {});
   expect(result).eq(src);
-})
+});
 
 test("parse line w/ single replacement", () => {
   const src = "var a = 4; // #replace 4=workSize";
-  const result = replacer(src, {workSize:128})
+  const result = replacer(src, { workSize: 128 });
   expect(result).includes("var a = 128;");
+});
+
+test("parse line w/ mulitple replacement", () => {
+  const src = "for (var a = 0; a <= 4; a++) { // #replace 0=start 4=end";
+  const result = replacer(src, { start: 128, end: 255 });
+  expect(result).includes("a = 128; a <= 255;");
 });
