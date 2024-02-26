272d75c04a2a88a9e87e06ffac615ee9e26f9342
lee mighdoll <lee@underneath.ca>
2024-02-26 14:21:14 -0800

    DRY vite config with baseViteConfig

--- packages/mini-parse/test-util.vite.config.ts
+++ packages/mini-parse/test-util.vite.config.ts
@@ -1,30 +1,36 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
-import dts from "vite-plugin-dts";
+import { LibraryOptions, defineConfig } from "vite";
 // import { visualizer } from "rollup-plugin-visualizer";
+import { baseViteConfig } from "./vite-base-config.js";
 
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    dts(), // generate
-    // visualizer({ brotliSize: true, gzipSize: true }), // generate stats.html size report
-  ],
-  build: {
-    emptyOutDir: false, 
-    lib: {
-      name: "mini-parse-testing",
-      formats: ["es"],
-      entry: [resolve(__dirname, "./src/test-util/index.ts")],
-      fileName: "testUtil"
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vitest', 'mini-parse'],
-    },
-    minify: false,
-    sourcemap: true,
-  },
-});
+const config = baseViteConfig();
+config.build!.emptyOutDir = false;
+(config.build!.lib as LibraryOptions).formats = ["es"];
+
+// config.plugins?.push(visualizer({ brotliSize: true, gzipSize: true })); // generate stats.html size report
+
+export default defineConfig(config);
+
+// {
+//   plugins: [
+//     tsconfigPaths(),
+//     dts(), // generate
+//     // visualizer({ brotliSize: true, gzipSize: true }), // generate stats.html size report
+//   ],
+//   build: {
+//     emptyOutDir: false,
+//     lib: {
+//       name: "mini-parse-testing",
+//       formats: ["es"],
+//       entry: [resolve(__dirname, "./src/test-util/index.ts")],
+//       fileName: "testUtil"
+//     },
+//     rollupOptions: {
+//       // make sure to externalize deps that shouldn't be bundled
+//       // into your library
+//       external: ['vitest', 'mini-parse'],
+//     },
+//     minify: false,
+//     sourcemap: true,
+//   },
+// });
--- packages/mini-parse/vite.config.ts
+++ packages/mini-parse/vite.config.ts
@@ -1,27 +1,9 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
-import dts from "vite-plugin-dts";
-// import { visualizer } from "rollup-plugin-visualizer";
+import { LibraryOptions, defineConfig } from "vite";
+import { baseViteConfig } from "./vite-base-config.js";
 
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    dts(), // generate 
-    // visualizer({ brotliSize: true, gzipSize: true }), // generate stats.html size report
-  ],
-  build: {
-    lib: {
-      name: "mini-parse",
-      entry: [
-        resolve(__dirname, "src/index.ts"),
-      ],
-    },
-    // minify: 'terser',
-    sourcemap: true,
-  },
-  test: {
-    setupFiles: "./src/test/TestSetup.ts",
-  },
-});
+const config = baseViteConfig();
+config.test = { setupFiles: "./src/test/TestSetup.ts" };
+(config.build.lib as LibraryOptions).name = "mini-parse";
+
+export default defineConfig(config);
