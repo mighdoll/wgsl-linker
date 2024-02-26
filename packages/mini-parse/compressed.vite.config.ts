119552ac924b5a17a213632bd260ff7a13b3fb73
lee mighdoll <lee@underneath.ca>
2024-02-26 15:12:35 -0800

    note that size estimate is a bit high 'cause we include tracing

--- packages/mini-parse/compressed.vite.config.ts
+++ packages/mini-parse/compressed.vite.config.ts
@@ -3,10 +3,13 @@ import { LibraryOptions, defineConfig } from "vite";
 // import { visualizer } from "rollup-plugin-visualizer";
 import { baseViteConfig } from "./vite-base-config.js";
 
+// Note that this will include the debug tracing code, so
+// so the size estimate is an overestimate of production size
+
 const config = baseViteConfig();
 config.build!.emptyOutDir = false;
+config.build!.minify = "terser";
 (config.build!.lib as LibraryOptions).formats = ["es", "cjs"];
-config.build!.minify = 'terser';
 (config.build!.lib as LibraryOptions).fileName = "minified";
 
 // config.plugins?.push(visualizer({ brotliSize: true, gzipSize: true })); // generate stats.html size report
