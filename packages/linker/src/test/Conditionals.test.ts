3d3eee72abff1fd0852fcef5fb55527395bb7de1
lee mighdoll <lee@underneath.ca>
2024-02-26 23:37:42 -0800

    more #if tests

--- packages/linker/src/test/Conditionals.test.ts
+++ packages/linker/src/test/Conditionals.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
 import { processConditionals } from "../Conditionals.js";
+import { expectNoLogErr } from "mini-parse/test-util";
 
 test("parse #if #endif", () => {
   const src = `
@@ -10,4 +11,63 @@ test("parse #if #endif", () => {
 
   const result = processConditionals(src, { foo: true });
   expect(result).contains("fn f() { }");
+});
+
+test("parse // #if !foo", () => {
+  const src = `
+    // #if !foo
+      fn f() { }
+    // #endif 
+    `;
+  const result = processConditionals(src, { foo: false });
+  expect(result).contains("fn f() { }");
+});
+
+test("parse #if !foo (true)", () => {
+  const src = `
+    // #if !foo
+      fn f() { }
+    // #endif 
+    `;
+  expectNoLogErr(() => {
+    const result = processConditionals(src, { foo: true });
+    expect(result).eq("\n");
+  });
+});
+
+test("parse #if !foo #else #endif", () => {
+  const src = `
+    // #if !foo
+      fn f() { notfoo(); }
+    // #else
+      fn g() { foo(); }
+    // #endif 
+    `;
+  const result = processConditionals(src, { foo: true });
+  expect(result).contains("fn g()");
+  expect(result).not.contains("fn f()");
+});
+
+test.skip("parse nested #if", () => {
+  const src = `
+    #if foo
+
+    #if bar
+      fn f() { }
+    #endif 
+
+    #if zap
+      fn zap() { }
+    #endif
+
+      fn g() { }
+    #endif 
+    `;
+  expectNoLogErr(() => {
+    const result = processConditionals(src, { foo: true, zap: true });
+    console.log(result);
+    expect(result).contains("fn zap()");
+    expect(result).contains("fn g()");
+    expect(result).not.contains("fn f()");
+  });
 });
