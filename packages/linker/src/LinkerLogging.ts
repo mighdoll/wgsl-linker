e4725a6f573d4615d5c319c55b8fa609949d5a60
lee mighdoll <lee@underneath.ca>
2024-02-29 22:16:34 -0800

    add moduleLog log variant

--- packages/linker/src/LinkerLogging.ts
+++ packages/linker/src/LinkerLogging.ts
@@ -1,11 +1,20 @@
 import { logger, srcLog } from "mini-parse";
 import { FoundRef } from "./TraverseRefs.js";
+import { TextModule } from "./ParseModule.js";
 
 export function refLog(ref: FoundRef, ...msgs: any[]): void {
   if (ref.kind !== "gen") {
-    const {src, srcMap: sourceMap} = ref.expMod;
-    srcLog(sourceMap ?? src, ref.elem.start, ...msgs);
+    moduleLog(ref.expMod, [ref.elem.start, ref.elem.end], ...msgs);
   } else {
     logger(ref.name, ...msgs);
   }
+}
+
+export function moduleLog(
+  mod: TextModule,
+  pos: number | [number, number],
+  ...msgs: any[]
+): void {
+  const { src, srcMap } = mod;
+  srcLog(srcMap ?? src, pos, ...msgs);
 }
