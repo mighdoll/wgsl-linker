a3eee0d90e3bad1d66a62a08486e3f466f271401
lee mighdoll <lee@underneath.ca>
2024-02-29 23:39:21 -0800

    rm unused

--- packages/mini-parse/test-util.vite.config.ts
+++ packages/mini-parse/test-util.vite.config.ts
@@ -10,27 +10,3 @@ config.build!.emptyOutDir = false;
 // config.plugins?.push(visualizer({ brotliSize: true, gzipSize: true })); // generate stats.html size report
 
 export default defineConfig(config);
-
-// {
-//   plugins: [
-//     tsconfigPaths(),
-//     dts(), // generate
-//     // visualizer({ brotliSize: true, gzipSize: true }), // generate stats.html size report
-//   ],
-//   build: {
-//     emptyOutDir: false,
-//     lib: {
-//       name: "mini-parse-testing",
-//       formats: ["es"],
-//       entry: [resolve(__dirname, "./src/test-util/index.ts")],
-//       fileName: "testUtil"
-//     },
-//     rollupOptions: {
-//       // make sure to externalize deps that shouldn't be bundled
-//       // into your library
-//       external: ['vitest', 'mini-parse'],
-//     },
-//     minify: false,
-//     sourcemap: true,
-//   },
-// });
