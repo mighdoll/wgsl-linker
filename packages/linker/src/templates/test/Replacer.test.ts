eb4653a6f120df7bc98faf599d67eeda17044ef1
lee mighdoll <lee@underneath.ca>
2024-03-04 21:23:30 -0800

    typo

--- packages/linker/src/templates/test/Replacer.test.ts
+++ packages/linker/src/templates/test/Replacer.test.ts
@@ -19,7 +19,7 @@ test("parse line w/ single replacement", () => {
   expect(result).includes("var a = 128;");
 });
 
-test("parse line w/ mulitple replacement", () => {
+test("parse line w/ multiple replacement", () => {
   const src = "for (var a = 0; a <= 4; a++) { // #replace 0=start 4=end";
   const result = replacer(src, { start: 128, end: 255 });
   expect(result).includes("a = 128; a <= 255;");
