13e0798b2fea314b8b348c4d8039397645eb2d75
lee mighdoll <lee@underneath.ca>
2024-04-28 22:21:36 -0700

    validate srcMaps generated by new sliceReplace

--- packages/linker/src/test/Slicer.test.ts
+++ packages/linker/src/test/Slicer.test.ts
@@ -1,52 +1,94 @@
+import { SrcMap } from "mini-parse";
 import { expect, test } from "vitest";
 import { sliceReplace2 } from "../Slicer.js";
-import { dlog } from "berry-pretty";
 
 test("slice middle", () => {
   const src = "aaabbbc";
   const srcMap = sliceReplace2(src, [{ start: 3, end: 6, replacement: "X" }]);
-  const {dest, entries} = srcMap;
+  const { dest, entries } = srcMap;
   expect(dest).eq("aaaXc");
-  dlog({entries});
+  expect(entries).toMatchInlineSnapshot(`
+    [
+      {
+        "destEnd": 3,
+        "destStart": 0,
+        "src": "aaabbbc",
+        "srcEnd": 6,
+        "srcStart": 0,
+      },
+      {
+        "destEnd": 4,
+        "destStart": 3,
+        "src": "aaabbbc",
+        "srcEnd": 6,
+        "srcStart": 6,
+      },
+      {
+        "destEnd": 5,
+        "destStart": 4,
+        "src": "aaabbbc",
+        "srcEnd": 7,
+        "srcStart": 6,
+      },
+    ]
+  `);
 });
 
 test("slice end", () => {
   const src = "aaabbb";
-  const { dest } = sliceReplace2(src, [{ start: 3, end: 6, replacement: "X" }]);
-  expect(dest).eq("aaaX");
+  const srcMap = sliceReplace2(src, [{ start: 3, end: 6, replacement: "X" }]);
+  expect(srcMap.dest).eq("aaaX");
+  validateDestCovered(srcMap);
 });
 
 test("slice beginning", () => {
   const src = "aaabbb";
-  const { dest } = sliceReplace2(src, [{ start: 0, end: 3, replacement: "X" }]);
-  expect(dest).eq("Xbbb");
+  const srcMap = sliceReplace2(src, [{ start: 0, end: 3, replacement: "X" }]);
+  validateDestCovered(srcMap);
+  expect(srcMap.dest).eq("Xbbb");
 });
 
 test("slice multiple", () => {
   const src = "aaabbbc";
-  const { dest } = sliceReplace2(src, [
+  const srcMap = sliceReplace2(src, [
     { start: 3, end: 6, replacement: "B" },
     { start: 0, end: 3, replacement: "A" },
   ]);
-  expect(dest).eq("ABc");
+  validateDestCovered(srcMap);
+  expect(srcMap.dest).eq("ABc");
+  expect(srcMap.entries).length(3);
 });
 
 test("slice none", () => {
   const src = "aaabbbc";
-  const { dest } = sliceReplace2(src, []);
-  expect(dest).eq(src);
+  const srcMap = sliceReplace2(src, []);
+  validateDestCovered(srcMap);
+  expect(srcMap.dest).eq(src);
 });
 
 test("slice none with start and end", () => {
   const src = "aaabbbc";
-  const { dest } = sliceReplace2(src, [], 3, 6);
-  expect(dest).eq("bbb");
+  const srcMap = sliceReplace2(src, [], 3, 6);
+  validateDestCovered(srcMap);
+  expect(srcMap.dest).eq("bbb");
 });
 
 test("slice one with start and end", () => {
   const src = "aaabbbc";
 
   const slices = [{ start: 3, end: 6, replacement: "B" }];
-  const { dest } = sliceReplace2(src, slices, 2);
-  expect(dest).eq("aBc");
+  const srcMap = sliceReplace2(src, slices, 2);
+  validateDestCovered(srcMap);
+  expect(srcMap.dest).eq("aBc");
 });
+
+/** verify that the srcMap covers every part of the destination text */
+function validateDestCovered(srcMap: SrcMap): void {
+  const { dest, entries } = srcMap;
+  let destPos = 0;
+  entries.forEach((e) => {
+    expect(e.destStart).eq(destPos);
+    destPos = e.destEnd;
+  });
+  expect(destPos).eq(dest.length);
+}
