75f3040c6a751f1a3c9d39fee4225debb366e383
lee mighdoll <lee@underneath.ca>
2024-04-29 22:05:36 -0700

    add merge for src maps

--- packages/mini-parse/src/SrcMap.ts
+++ packages/mini-parse/src/SrcMap.ts
@@ -16,13 +16,13 @@ export class SrcMap {
   entries: SrcMapEntry[];
   dest: string;
 
-  constructor(dest: string, entries:SrcMapEntry[] = []) {
+  constructor(dest: string, entries: SrcMapEntry[] = []) {
     this.dest = dest;
     this.entries = entries;
   }
 
   /** add a new mapping from src to dest ranges.
-   * entries must be non-overlapping, and added in order.
+   * entries must be non-overlapping in the destination
    */
   addEntries(newEntries: SrcMapEntry[]): void {
     this.entries.push(...newEntries);
@@ -31,7 +31,7 @@ export class SrcMap {
   /** given positions in the dest string,
    * @return corresponding positions in the src strings */
   mapPositions(...positions: number[]): SrcPosition[] {
-    return positions.map((p) => mapPos(p, this.entries));
+    return positions.map((p) => destToSrc(p, this.entries));
   }
 
   /** internally compress adjacent entries where possible */
@@ -57,13 +57,73 @@ export class SrcMap {
     }
     this.entries = newEntries;
   }
+
+  /** sort in destination order */
+  sort(): void {
+    this.entries.sort((a, b) => a.destStart - b.destStart);
+  }
+
+  /** This SrcMap's destination is a src for the other srcmap,
+   * so combine the two and return the result.
+   *
+   */
+  merge(other: SrcMap): SrcMap {
+    const mappedEntries = other.entries.filter((e) => e.src === this.dest);
+    if (mappedEntries.length === 0) {
+      console.error("other source map does not link to this one");
+      return this;
+    }
+    sortSrc(mappedEntries)
+    const newEntries = this.entries.map(e => {
+      const mappedStart =  srcToDest(e.destStart, mappedEntries)
+      const mappedEnd =  srcToDest(e.destEnd, mappedEntries)
+      const newEntry:SrcMapEntry = {
+        src: e.src,
+        srcStart: e.srcStart,
+        srcEnd: e.srcStart,
+        destStart: mappedStart.position,
+        destEnd: mappedEnd.position,
+      }
+      return newEntry;
+    });
+
+    const otherSources = other.entries.filter((e) => e.src !== this.dest);
+
+    const newMap = new SrcMap(other.dest, [...otherSources, ...newEntries]);
+    newMap.sort();
+    return newMap;
+  }
 }
 
-function mapPos(pos: number, entries: SrcMapEntry[]): SrcPosition {
-  const entry = entries.find((e) => e.destStart <= pos && e.destEnd >= pos);
-  if (!entry) throw new Error(`no SrcMapEntry for pos: ${pos}`);
+/** sort entries in place by src start position */
+function sortSrc(entries: SrcMapEntry[]): void {
+    entries.sort((a, b) => a.srcStart - b.srcStart);
+}
+
+/** 
+ * @param entries should be sorted in destStart order
+ * @return the source position corresponding to a provided destination position 
+ * 
+*/
+function destToSrc(destPos: number, entries: SrcMapEntry[]): SrcPosition {
+  const entry = entries.find((e) => e.destStart <= destPos && e.destEnd >= destPos);
+  if (!entry) throw new Error(`no SrcMapEntry for dest position: ${destPos}`);
   return {
     src: entry.src,
-    position: entry.srcStart + pos - entry.destStart,
+    position: entry.srcStart + destPos - entry.destStart,
+  };
+}
+
+/** 
+ * @param entries should be sorted in srcStart order
+ * @return the dest position corresponding to a provided src position
+ * 
+*/
+function srcToDest(pos: number, entries: SrcMapEntry[]): SrcPosition {
+  const entry = entries.find((e) => e.srcStart <= pos && e.srcEnd >= pos);
+  if (!entry) throw new Error(`no SrcMapEntry for src pos: ${pos}`);
+  return {
+    src: entry.src,
+    position: entry.destStart + pos - entry.srcStart,
   };
 }
--- packages/mini-parse/src/test/SrcMap.test.ts
+++ packages/mini-parse/src/test/SrcMap.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
 import { SrcMap } from "../SrcMap.js";
+import {dlog} from "berry-pretty";
 
 test("compact", () => {
   const src = "a b";
@@ -23,3 +24,50 @@ test("compact", () => {
     ]
   `);
 });
+
+
+test("merge", () => {
+  const src = "a b";
+  const src2 = " d";
+  const mid = "|" + src + src2;
+  const dest = "xx" + mid + " z";
+  /*
+    mid:
+      01234567890
+      |a b d
+    dest:
+      01234567890
+      xx|a b d z
+  */
+
+  const map1 = new SrcMap(mid, [
+    { src, srcStart: 0, srcEnd: 3, destStart: 1, destEnd: 4 },
+  ]);
+
+  const map2 = new SrcMap(dest, [
+    { src: mid, srcStart: 1, srcEnd: 5, destStart: 3, destEnd: 7 },
+    { src: src2, srcStart: 0, srcEnd: 2, destStart: 7, destEnd: 9 },
+  ]);
+
+  const merged = map1.merge(map2);
+  dlog(merged.entries);
+  expect(merged.entries).toMatchInlineSnapshot(`
+    [
+      {
+        "destEnd": 6,
+        "destStart": 3,
+        "src": "a b",
+        "srcEnd": 0,
+        "srcStart": 0,
+      },
+      {
+        "destEnd": 9,
+        "destStart": 7,
+        "src": " d",
+        "srcEnd": 2,
+        "srcStart": 0,
+      },
+    ]
+  `); 
+
+})
