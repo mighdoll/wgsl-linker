7eb1809bb5007c59a7108574cc16c2410f10b1d7
lee mighdoll <lee@underneath.ca>
2024-06-27 15:47:55 -0700

    optional notes field

--- packages/shared-tests/src/test-cases/TestSchema.ts
+++ packages/shared-tests/src/test-cases/TestSchema.ts
@@ -1,4 +1,5 @@
 export interface WgslTestSrc {
   name: string;                 // human readable description of test
-  src: Record<string, string>;  // source wgsl+ texts 
+  src: Record<string, string>;  // source wgsl+ texts, keys are file paths
+  notes?: string;               // additional notes to test implementors
 }
