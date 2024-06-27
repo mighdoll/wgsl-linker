658d90c8147d8efa18ac8edfa28ebd9d5f4e34d8
lee mighdoll <lee@underneath.ca>
2024-06-27 11:37:55 -0700

    simple tool to convert tests to json

--- packages/linker/src/test/shared/TestSchema.ts
+++
@@ -1,4 +0,0 @@
-export interface WgslTestSrc {
-  name: string;                 // human readable description of test
-  src: Record<string, string>;  // source wgsl+ texts 
-}
---
+++ packages/shared-tests/src/tools/tests-to-json.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import fs from "fs";
+import {importTests} from "../test-cases/ImportTests.js";
+
+main();
+
+async function main() : Promise<void> {
+  const json = JSON.stringify(importTests, null, 2);
+  fs.writeFileSync("./src/test-cases/import-tests.json", json); 
+}
