b88dcc9ac1128e53fdf0fabe4ee819ab16d8d078
lee mighdoll <lee@underneath.ca>
2024-06-26 22:40:49 -0700

    import from ./bar works now

--- packages/linker/src/test/shared/ImportTests.ts
+++ packages/linker/src/test/shared/ImportTests.ts
@@ -5,7 +5,7 @@ export const linkerTests: WgslTestSrc[] = [
     name: "#import foo",
     src: {
       "./main.wgsl": `
-          #import foo
+          #import foo from ./bar
           fn main() {
             foo();
           }
