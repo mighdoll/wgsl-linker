a8ba9dd9b4db6a1ec6d7b57f5e31deacadc978e2
lee mighdoll <lee@underneath.ca>
2024-07-09 10:47:50 -0700

    mv to separate .snap tests for clarity

--- packages/linker/src/test/RustDirective.test.ts
+++ packages/linker/src/test/RustDirective.test.ts
@@ -9,341 +9,47 @@ const testRustImport = tokens(argsTokens, rustImport);
 
 test("import a::b::c", (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 14,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "a",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "b",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "c",
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
-//    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
 test("import my::lighting::pbr;", (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 25,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "my",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "lighting",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "pbr",
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
 test("import my::lighting::{ pbr };", (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 29,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "my",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "lighting",
-            },
-            SegmentList {
-              "list": [
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "pbr",
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
 test(`import my::lighting::{ pbr, jelly };`, (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 36,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "my",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "lighting",
-            },
-            SegmentList {
-              "list": [
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "pbr",
-                    },
-                  ],
-                },
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "jelly",
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
 test("import my::lighting::{ pbr, jelly::jam };", (ctx) => {
-  const { parsed, appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 41,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "my",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "lighting",
-            },
-            SegmentList {
-              "list": [
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "pbr",
-                    },
-                  ],
-                },
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "jelly",
-                    },
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "jam",
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  const { appState } = testAppParse(testRustImport, ctx.task.name);
+  expect(appState).toMatchSnapshot();
 });
 
 test("import my::lighting::{ pbr as lights }", (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 38,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "my",
-            },
-            SimpleSegment {
-              "as": undefined,
-              "name": "lighting",
-            },
-            SegmentList {
-              "list": [
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": "lights",
-                      "name": "pbr",
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
 test("import a::*", (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 11,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "a",
-            },
-            Wildcard {
-              "wildcard": "*",
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
 test("import a::{b, c}::*", (ctx) => {
   const { appState } = testAppParse(testRustImport, ctx.task.name);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 19,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "a",
-            },
-            SegmentList {
-              "list": [
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "b",
-                    },
-                  ],
-                },
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "c",
-                    },
-                  ],
-                },
-              ],
-            },
-            Wildcard {
-              "wildcard": "*",
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
 
 test("multiline import", () => {
   const src = `import a::
         {b, c}::*;`;
   const { appState } = testAppParse(testRustImport, src);
-  expect(appState).toMatchInlineSnapshot(`
-    [
-      {
-        "end": 29,
-        "imports": ImportTree {
-          "segments": [
-            SimpleSegment {
-              "as": undefined,
-              "name": "a",
-            },
-            SegmentList {
-              "list": [
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "b",
-                    },
-                  ],
-                },
-                ImportTree {
-                  "segments": [
-                    SimpleSegment {
-                      "as": undefined,
-                      "name": "c",
-                    },
-                  ],
-                },
-              ],
-            },
-            Wildcard {
-              "wildcard": "*",
-            },
-          ],
-        },
-        "kind": "treeImport",
-        "start": 0,
-      },
-    ]
-  `);
+  expect(appState).toMatchSnapshot();
 });
