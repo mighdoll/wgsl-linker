f09bc277c0341f9ba0882e054d0d106a2a3b5e81
lee mighdoll <lee@underneath.ca>
2024-07-17 20:17:19 -0700

    mv resolveImport tests to their own file

--- packages/linker/src/test/ResolveImportTree.test.ts
+++ packages/linker/src/test/ResolveImportTree.test.ts
@@ -2,7 +2,6 @@ import { expect, test } from "vitest";
 import { ModuleRegistry } from "../ModuleRegistry.js";
 import { TextExport, TextModule } from "../ParseModule.js";
 import { importResolutionMap } from "../ImportResolutionMap.js";
-import { resolveImport } from "../ResolveImport.js";
 
 test("simple tree", () => {
   const registry = new ModuleRegistry({
@@ -38,57 +37,6 @@ test("simple tree", () => {
   expect(expSegments).deep.eq(["bar", "foo"]);
 });
 
-test("matchImport simple completing path", () => {
-  const registry = new ModuleRegistry({
-    wgsl: {
-      "main.wgsl": `
-         import bar::foo;
-
-         module main
-         fn main() { foo(); }
-      `,
-      "bar.wgsl": `
-         module bar
-
-         export fn foo() { }
-        `,
-    },
-  });
-  const parsedModules = registry.parsed();
-  const impMod = parsedModules.moduleByPath(["main"]) as TextModule;
-  const treeImports = impMod.imports.filter((i) => i.kind === "treeImport");
-  const resolveMap = importResolutionMap(impMod, treeImports, parsedModules);
-
-  const found = resolveImport("foo", resolveMap);
-  expect(found).toBeDefined();
-  expect(found?.module.name).eq("bar");
-  expect((found?.exp as TextExport).ref.name).eq("foo");
-});
-
-test("matchImport bar::foo()", () => {
-  const registry = new ModuleRegistry({
-    wgsl: {
-      "main.wgsl": `
-         import bar::foo;
-         module main
-         fn main() { bar::foo(); }
-      `,
-      "bar.wgsl": `
-         module bar
-
-         export fn foo() { }
-        `,
-    },
-  });
-  const parsedModules = registry.parsed();
-  const impMod = parsedModules.moduleByPath(["main"]) as TextModule;
-  const treeImports = impMod.imports.filter((i) => i.kind === "treeImport");
-  const resolveMap = importResolutionMap(impMod, treeImports, parsedModules);
-  const found = resolveImport("bar::foo", resolveMap);
-  expect(found).toBeDefined();
-  expect(found?.module.name).eq("bar");
-  expect((found?.exp as TextExport).ref.name).eq("foo");
-});
 
 test.skip("tree with path segment list");
 test.skip("tree with trailing wildcard");
