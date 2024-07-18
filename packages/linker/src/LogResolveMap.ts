40ebc73680f0db71885eda17c34fd64f28737e3d
lee mighdoll <lee@underneath.ca>
2024-07-18 16:29:15 -0700

    expose some more resolve map string utils for tests

--- packages/linker/src/LogResolveMap.ts
+++ packages/linker/src/LogResolveMap.ts
@@ -3,11 +3,19 @@ import { ResolveMap } from "./ImportResolutionMap.js";
 import { TextExport } from "./ParseModule.js";
 
 export function logResolveMap(resolveMap: ResolveMap): void {
-  const expMap = [...resolveMap.exportMap.entries()].map(([imp, exp]) => {
-    return `${imp} -> ${exp.module.name}/${(exp.exp as TextExport).ref.name}`;
-  });
-  const pathMap = [...resolveMap.pathsMap.entries()].map(([imp, exp]) => {
+  const expMap = exportsToStrings(resolveMap);
+  const pathMap = pathsToStrings(resolveMap);
+  dlog({ expMap, pathMap });
+}
+
+export function pathsToStrings(resolveMap: ResolveMap): string[] {
+  return [...resolveMap.pathsMap.entries()].map(([imp, exp]) => {
     return `${imp.join("/")} -> ${exp.join("/")}`;
   });
-  dlog({ expMap, pathMap });
+}
+
+export function exportsToStrings(resolveMap: ResolveMap): string[] {
+  return [...resolveMap.exportMap.entries()].map(([imp, exp]) => {
+    return `${imp} -> ${exp.module.name}/${(exp.exp as TextExport).ref.name}`;
+  });
 }
