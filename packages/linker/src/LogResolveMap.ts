46cc226a6f628b6e1abad6c20e8b791f097adf73
lee mighdoll <lee@underneath.ca>
2024-08-11 10:59:00 -0700

    update debug

--- packages/linker/src/LogResolveMap.ts
+++ packages/linker/src/LogResolveMap.ts
@@ -4,18 +4,21 @@ import { TextExport } from "./ParseModule.js";
 
 export function logResolveMap(resolveMap: ResolveMap): void {
   const expMap = exportsToStrings(resolveMap);
-  const pathMap = pathsToStrings(resolveMap);
-  dlog({ expMap, pathMap });
+  const pathsMap = pathsToStrings(resolveMap);
+  dlog({ expMap});
+  dlog({ pathsMap });
 }
 
 export function pathsToStrings(resolveMap: ResolveMap): string[] {
-  return [...resolveMap.pathsMap.entries()].map(([imp, exp]) => {
+  return [...resolveMap.pathsMap].map(([imp, exp]) => {
     return `${imp.join("/")} -> ${exp.join("/")}`;
   });
 }
 
 export function exportsToStrings(resolveMap: ResolveMap): string[] {
-  return [...resolveMap.exportMap.entries()].map(([imp, exp]) => {
-    return `${imp} -> ${exp.module.name}/${(exp.exp as TextExport).ref.name}`;
+  return [...resolveMap.exportMap].map(([imp, exp]) => {
+    const expPath = `${exp.modExp.module.name}/${(exp.modExp.exp as TextExport).ref.name}`;
+    const expImpArgs = exp.expImpArgs ? `(${exp.expImpArgs.join(", ")})` : "";
+    return `${imp} -> ${expPath} (${expImpArgs})`;
   });
 }
