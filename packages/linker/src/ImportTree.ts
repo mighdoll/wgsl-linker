50b0ab4fdee9a4c65e9dd9b250c03f07a00feb8e
lee mighdoll <lee@underneath.ca>
2024-08-12 12:48:19 -0700

    debug ImportTree toString for tests

--- packages/linker/src/ImportTree.ts
+++ packages/linker/src/ImportTree.ts
@@ -20,3 +20,26 @@ export class Wildcard {
   public static _ = new Wildcard();
   wildcard = "*"; // to identify this object in debug logging
 }
+
+export function treeToString(tree: ImportTree): string {
+  return tree.segments.map((s) => segmentToString(s)).join("/");
+}
+
+function segmentToString(segment: PathSegment): string {
+  if (segment instanceof SimpleSegment) {
+    const {name, as, args} = segment;
+    const asMsg = as ? ` as ${as}` : "";
+    const argsMsg = args ? `(${args.join(", ")})` : "";
+    return `${name}${argsMsg}${asMsg}`;
+  }
+  if (segment instanceof Wildcard) {
+    return "*";
+  }
+  if (segment instanceof SegmentList) {
+    return `{${segment.list.map((s) => segmentToString(s)).join(", ")}}`;
+  }
+  if (segment instanceof ImportTree) {
+    return `(${treeToString(segment)})`;
+  }
+  throw new Error(`unknown segment type ${segment}`);
+}
